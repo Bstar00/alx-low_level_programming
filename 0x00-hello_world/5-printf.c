9c3139c7f3653c9f4569e617e803a1e58b91d17b
Orio-Ola <samueloriola212@gmail.com>
2022-09-08 08:12:33 -0700

    Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
-*main - prints the string in the printf function
+*main - Entry point
 *Description: using the mian function
 *this program prints with proper grammar, but the outcome is a piece of art,
-*return 0 after printing the function
+*return 0
 */
 int main(void)
 {         
         printf("with proper grammar, but the outcome is a piece of art,\n");
         return (0);
+}
