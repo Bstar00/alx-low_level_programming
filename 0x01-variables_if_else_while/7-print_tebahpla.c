5a70a9c3d48755b1d8e6fc9686f18a43539ce60b
Orio-Ola <samueloriola212@gmail.com>
2022-09-09 09:18:11 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of a function
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{	char c;
+	for (c ='2'; c >= 'a'; c--)
+	{	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
