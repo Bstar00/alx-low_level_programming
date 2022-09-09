7c66bd69cc380be7794927c99496d5017e56cef1
Orio-Ola <samueloriola212@gmail.com>
2022-09-09 11:59:08 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 /**
- * main - Entry point of function 
+ * main - Entry point of function
  *
- * Return: Always 0 (success)
+ * Return : Always 0 (success)
  */
 int main(void)
 {	int i, j;
 	for (i = 0; i <= 9; i++)
 	{	for (j = 1; j <= 9; j++)
-		{	if (j > i)
-			{	putchar(i +'0');
-				putchar(j +'0');
-				if (i != 8)
-				{	putchar(',');
-					putchar(' ');
-				}
-			}
+	{	if (j > i)
+	{	putchar(i + '0');
+		putchar(j + '0');
+		if (i != 8)
+		{	putchar(',');
+			putchar(' ');
 		}
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
