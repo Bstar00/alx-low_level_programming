5dd8b8cfe35d4d96da4d90ccc1a89909ff459805
Orio-Ola <samueloriola212@gmail.com>
2022-09-09 14:53:15 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,8 +8,8 @@
 int main(void)
 {	int i, j;
 	for (i = 0; i <= 9; i++)
-	{	for (j = 1; j <= 9; j++)
-	{	if (j > i)
+		{	for (j = 1; j <= 9; j++)
+		{	if (j > i)
 		{	putchar(i + '0');
 			putchar(j + '0');
 			if (i != 8)
@@ -17,8 +17,8 @@ int main(void)
 			putchar(' ');
 		}	
 		}	
-	}
-	}
+		}
+		}
 	putchar('\n');
 	return (0);
 }
