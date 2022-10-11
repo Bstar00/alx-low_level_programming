80f4bce0add508c10702b37e16984f5f38e75471
Orio-Ola <samueloriola212@gmail.com>
2022-10-11 02:31:04 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -64,6 +64,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	doggo->name = _strcopy(doggo->name, name);
 	doggo->age = age;
-	doggo->owner = _strcopy(doggo->, owner);
+	doggo->owner = _strcopy(doggo->owner, owner);
 	return (doggo);
 }
