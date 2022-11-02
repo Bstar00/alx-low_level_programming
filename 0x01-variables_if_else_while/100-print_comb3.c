ff74592c28c16541aa4613b03e70c42cf1d770c7
Orio-Ola <samueloriola212@gmail.com>
2022-11-02 13:03:39 -0700

    Advance task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
+
 /**
- * main - Entry point of a function
+ * main - Print combinations of two digit numbers
  *
- * Description: prints single digit numbers starting from 0
- * Return: returns 0 at the end (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
-{	int i, j;
-	for (i = 0; i <= 9; i++)
-{	for (j = 1; j <= 9; j++)
-	{	if (j > i)
-		{	putchar(i + '0');
-			putchar(j + '0');
-			if (i != 8)
-			{	putchar(',');
+{
+	int tens;
+	int ones;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1; ones <= 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+			if (tens < 8)
+			{
+				putchar(',');
 				putchar(' ');
 			}
 		}
-
 	}
-}
 	putchar('\n');
 	return (0);
 }
