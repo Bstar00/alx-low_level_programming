a5107e4ae628f4f3506cc550d5433709de4ac0a7
Orio-Ola <samueloriola212@gmail.com>
2023-12-20 17:51:09 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,23 +7,23 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-	int b;
+    int i;
+    int b;
 
-	if (n > 0)
-	{
-		for (i = 0; i < n; i++)
-		{
-			for (b = 0; b < i; b++)
-			{
-				_putchar (' ');
-			}
-			_putchar (92);
-			_putchar ('\n');
-		}
-	}
-	else
-	{
-		_putchar ('\n');
-	}
+    if (n > 0)
+    {
+        for (i = 0; i < n; i++)
+        {
+            for (b = 0; b < i; b++)
+            {
+                _putchar(' '); // Print spaces before the backslash
+            }
+            _putchar(92); // Print the backslash character
+            _putchar('\n'); // Move to the next line
+        }
+    }
+    else
+    {
+        _putchar('\n'); // If n is not greater than 0, just print a newline
+    }
 }
