ef974e0bc5d9cd1bbb50736418f1919b2f996372
Orio-Ola <samueloriola212@gmail.com>
2023-12-20 13:12:21 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,16 +2,15 @@
 #include "main.h"
 
 /**
-* print_line - prints a straight line
-* @n: parameter
-* Return: returns nothing
-*/
-
+ * print_line - prints a straight line of underscores
+ * @n: length of the line
+ * Return: returns nothing
+ */
 void print_line(int n)
 {
-	while (n-- > 0)
-	{
-		putchar('_');
-	}
-	putchar('\n');
+    while (n-- > 0)
+    {
+        putchar('_');
+    }
+    putchar('\n');
 }
