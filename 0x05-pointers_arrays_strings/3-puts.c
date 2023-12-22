1bb8e73538b45a9c341a9f8cbafc6a69a83d1986
Orio-Ola <samueloriola212@gmail.com>
2023-12-22 22:52:10 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,20 +2,22 @@
 #include "main.h"
 
 /**
-* _puts - function to print a string
-* @str :string to print
-* Description: prints a string
-* On success: returns no error
-*/
-
+ * _puts - Prints a string followed by a newline.
+ * @str: The string to be printed.
+ *
+ * Description: This function prints each character of the string
+ * followed by a newline character.
+ */
 void _puts(char *str)
 {
-	int i = 0;
+    int i = 0;
 
-	while (*(str + i) != '\0')
-	{
-		putchar(*(str + i));
-		i++;
-	}
-	putchar(10);
+    // Iterate through the string until the null terminator is encountered
+    while (*(str + i) != '\0')
+    {
+        putchar(*(str + i));  // Print each character
+        i++;
+    }
+
+    putchar('\n');  // Print a newline character at the end
 }
