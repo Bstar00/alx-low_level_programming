81bcaba0a88dcff24d28ee54e6bcabf4f322104d
Orio-Ola <samueloriola212@gmail.com>
2023-12-22 22:46:02 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,37 +3,23 @@
 /**
  * main - entry point
  *
- * Return: 0
+ * Description: Prints numbers from 1 to 100 with "Fizz" for multiples of 3,
+ *              "Buzz" for multiples of 5, and "FizzBuzz" for multiples of both.
+ *
+ * Return: 0 (success)
  */
 int main(void)
 {
-	int p = 100;
-	int i;
+    int p = 100;  // Set the range up to 100
 
-	i = 1;
-	while (i <= p)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%i ", i);
-		}
-		i++;
-	}
+    // Iterate through numbers from 1 to 100
+    for (int i = 1; i <= p; ++i) {
+        // Use ternary operator to conditionally print "Fizz", "Buzz", or the number
+        printf("%s%s%s%s%i ", (i % 3 == 0) ? "Fizz" : "", (i % 5 == 0) ? "Buzz" : "",
+               (i % 3 != 0 && i % 5 != 0) ? "" : " ", (i % 3 != 0 && i % 5 != 0) ? "" : " ", i);
+    }
 
-	// Print "Buzz" without a space after the loop
-	printf("Buzz\n");
+    printf("\n");  // Print newline at the end
 
-	return (0);
+    return 0;  // Return success
 }
