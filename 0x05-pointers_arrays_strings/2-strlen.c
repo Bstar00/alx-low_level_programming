0ff6b2922702d7744b116553096ed3becb301b61
Orio-Ola <samueloriola212@gmail.com>
2023-12-22 22:50:55 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
-* _strlen - it returns the length of a string
-* @s: string
-* Return: returns length as integer;
-*/
-
+ * _strlen - Calculates the length of a string.
+ * @s: The string to find the length of.
+ *
+ * Return: The length of the string.
+ */
 int _strlen(char *s)
 {
-	int len = 0;
+    int len = 0;
 
-	while (*(s + len) != '\0')
-		len++;
-	return (len);
+    // Iterate through the string until the null terminator is encountered
+    while (*(s + len) != '\0')
+        len++;
+
+    return (len);  // Return the length of the string
 }
